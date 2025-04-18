aa54b9eb7d0ed00b7bf22621071e96ebd017e40a
Garegin Mazmanyan <garegin.ma@gmail.com>
2025-04-18 15:59:30 -0700

    fix typo

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
 """
 NevIR dataset module for negation in information retrieval.
 """
-from .data import load_nevir_dataset, get_example_data
+from .data import load_nevir_dataset
 
-__all__ = ["load_nevir_dataset", "get_example_data"]
+__all__ = ["load_nevir_dataset"]
